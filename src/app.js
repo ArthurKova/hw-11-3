c42d9c7d24363e36b146c34ce96ad0b7c2abf87a
ArthurKova <metaloutcome@gmail.com>
2023-06-21 16:45:25 +0300

    complete counter

--- src/app.js
+++ src/app.js
@@ -1,43 +1,29 @@
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+class CountdownTimer {
+  constructor(params) {
+    this.params = params;
+  }
 
-const makeTransaction = (transaction, onSuccess, onError) => {
-  const delay = randomIntegerFromInterval(200, 500);
+  startPageCount() {
+    setInterval(() => {
+      let time = this.params.targetDate - Date.now();
+      const pageRef = document.querySelector(this.params.selector);
+      const pageTimeRef = pageRef.querySelectorAll(".value");
 
-  setTimeout(() => {
-    const canProcess = Math.random() > 0.3;
+      pageTimeRef[0].textContent = Math.floor(time / (1000 * 60 * 60 * 24));
+      pageTimeRef[1].textContent = Math.floor(
+        (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      pageTimeRef[2].textContent = Math.floor(
+        (time % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      pageTimeRef[3].textContent = Math.floor((time % (1000 * 60)) / 1000);
+    }, 1000);
+  }
+}
 
-    if (canProcess) {
-      onSuccess(transaction.id, delay);
-    } else {
-      onError(transaction.id);
-    }
-  }, delay);
-};
+const newTimer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Jul 17, 2023"),
+});
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
-};
-
-const logError = (id) => {
-  console.warn(`Error processing transaction ${id}. Please try again later.`);
-};
-
-/*
- * Работает так
- */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-/*
- * Должно работать так
- */
-makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
-
-makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
-
-makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
-
-makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+// newTimer.startPageCount();
